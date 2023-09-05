80b28be687566f074a0ca40a8c08c6cacc8e6b0a
Amit Yadav <amy29981@gmail.com>
2023-09-05 23:24:27 +0530

    add state and messages

--- contracts/volume/src/msg.rs
+++ contracts/volume/src/msg.rs
@@ -1,20 +1,14 @@
-use std::fmt;
-
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cw20::Cw20Coin;
-
-use cosmwasm_std::{Binary, Coin, Timestamp, Uint128};
-
-use crate::state::{AtomicSwapOrder, Status};
+use cosmwasm_std::{Uint128};
 
 #[derive(Serialize, Deserialize, JsonSchema)]
 pub struct InstantiateMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub enum ExecuteMsg {
-    AddValue {
+    LogObservation {
         amount_traded: Uint128
     },
     SetContract {
@@ -30,52 +24,31 @@ pub struct MigrateMsg {}
 pub enum QueryMsg {
     /// Returns volume between specific interval
     VolumeInterval {
-        start_after: Option<String>,
-        limit: Option<u32>,
+        start: u64,
+        end: u64,
     },
     /// Returns total volume till latest timestamp
     TotalVolume {},
-    ListByDesiredTaker {
-        start_after: Option<String>,
-        limit: Option<u32>,
-        desired_taker: String,
+    /// Returns total volume till given timestamp
+    TotalVolumeAt {
+        timestamp: u64
     },
-    ListByMaker {
-        start_after: Option<String>,
-        limit: Option<u32>,
-        maker: String,
-    },
-    ListByTaker {
-        start_after: Option<String>,
-        limit: Option<u32>,
-        taker: String,
-    },
-    /// Returns the details of the named swap, error if not created.
-    /// Return type: DetailsResponse.
-    Details { id: String },
-    BidDetailsbyOrder {
-        start_after: Option<String>,
-        limit: Option<u32>,
-        order_id: String,
-    },
-    BidDetailsbyBidder {
-        order_id: String,
-        bidder: String,
-    }
+    /// Returns contract address for which volume is tracked
+    Contract {},
 }
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub struct DetailsResponse {
-    pub id: String,
-    pub maker: MakeSwapMsg,
-    pub status: Status,
-    pub path: String,
-    pub taker: Option<TakeSwapMsg>,
-    pub cancel_timestamp: Option<Timestamp>,
-    pub complete_timestamp: Option<Timestamp>,
-}
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub struct ListResponse {
-    /// List all open swap ids
-    pub swaps: Vec<AtomicSwapOrder>,
-}
+// #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+// pub struct DetailsResponse {
+//     pub id: String,
+//     pub maker: MakeSwapMsg,
+//     pub status: Status,
+//     pub path: String,
+//     pub taker: Option<TakeSwapMsg>,
+//     pub cancel_timestamp: Option<Timestamp>,
+//     pub complete_timestamp: Option<Timestamp>,
+// }
+// #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+// pub struct ListResponse {
+//     /// List all open swap ids
+//     pub swaps: Vec<AtomicSwapOrder>,
+// }
--- contracts/volume/src/state.rs
+++ contracts/volume/src/state.rs
@@ -1,123 +1,17 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use crate::msg::{MakeSwapMsg, TakeSwapMsg};
-use cosmwasm_std::{IbcEndpoint, StdResult, Storage, Timestamp, Coin};
-use cw_storage_plus::{Map, Item};
-
-pub const CHANNEL_INFO: Map<&str, ChannelInfo> = Map::new("channel_info");
+use cw_storage_plus::{Item};
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub struct ChannelInfo {
-    /// id of this channel
-    pub id: String,
-    /// the remote channel/port we connect to
-    pub counterparty_endpoint: IbcEndpoint,
-    /// the connection this exists on (you can use to query client/consensus info)
-    pub connection_id: String,
+pub struct Observation {
+    // timestamp
+    pub block_timestamp: u64,
+    // Number of observations till block_timestamp
+    pub num_of_observations: u64,
+    // volume cumulative
+    pub volume: u64
 }
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
-pub enum Status {
-    Initial,  // initialised on maker chain
-    Sync,     // synced to the taker chain
-    Cancel,   // cancelled
-    Complete, // completed
-}
-
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
-pub enum Side {
-    Native,
-    Remote,
-}
-
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub struct AtomicSwapOrder {
-    pub id: String,
-    pub side: Side,
-    pub maker: MakeSwapMsg,
-    pub status: Status,
-    // an IBC path, define channel and port on both Maker Chain and Taker Chain
-    pub path: String,
-    pub taker: Option<TakeSwapMsg>,
-    // In seconds
-    pub create_timestamp: u64,
-    pub cancel_timestamp: Option<Timestamp>,
-    pub complete_timestamp: Option<Timestamp>,
-}
-
-pub const SWAP_ORDERS: Map<u64, AtomicSwapOrder> = Map::new("swap_order");
-pub const ORDER_TO_COUNT: Map<&str, u64> = Map::new("order_to_count");
-pub const COUNT: Item<u64> = Item::new("count");
-
-// append order to end of list
-pub fn append_atomic_order(storage: &mut dyn Storage, order_id: &str, order: &AtomicSwapOrder) -> StdResult<u64> {
-    let count = COUNT.load(storage)?;
-
-    SWAP_ORDERS.save(storage, count, order)?;
-    ORDER_TO_COUNT.save(storage, &order_id, &count)?;
-    COUNT.save(storage, &(count + 1))?;
-    
-    Ok(count)
-}
-
-// set specific order
-pub fn set_atomic_order(storage: &mut dyn Storage, order_id: &str, order: &AtomicSwapOrder) -> StdResult<u64> {
-    let id = ORDER_TO_COUNT.load(storage, order_id)?;
-    SWAP_ORDERS.save(storage, id, order)?;
-    Ok(id)
-}
-
-// set specific order
-pub fn get_atomic_order(storage: &dyn Storage, order_id: &str) -> StdResult<AtomicSwapOrder> {
-    let id = ORDER_TO_COUNT.load(storage, order_id)?;
-    let swap_order = SWAP_ORDERS.load(storage, id)?;
-    Ok(swap_order)
-}
-
-// set specific order
-pub fn remove_atomic_order(storage: &mut dyn Storage, order_id: &str) -> StdResult<u64> {
-    let id = ORDER_TO_COUNT.load(storage, order_id)?;
-    SWAP_ORDERS.remove(storage, id);
-    Ok(id)
-}
-
-pub fn move_order_to_bottom(storage: &mut dyn Storage, order_id: &str) -> StdResult<u64> {
-    // Step 1: Retrieve the item based on the given ID.
-    let count = COUNT.load(storage)?;
-    let id: u64 = ORDER_TO_COUNT.load(storage, order_id)?;
-    let swap_order = SWAP_ORDERS.load(storage, id)?;
-    // Step 2: Remove the item from its current position.
-    SWAP_ORDERS.remove(storage, id);
-    // Step 3: Append the item to the end.
-    SWAP_ORDERS.save(storage, count, &swap_order)?;
-    ORDER_TO_COUNT.save(storage, &order_id, &count)?;
-    COUNT.save(storage, &(count + 1))?;
-    Ok(id)
-}
-
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub enum BidStatus {
-    Cancelled,
-    Executed,
-    Placed,
-}
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-pub struct Bid {
-    pub bid: Coin,
-    pub status: BidStatus,
-    pub bidder: String,
-    pub bidder_receiver: String,
-}
-// Map for order id -> Vec<Bids>
-// Order_id + BID_COUNT
-pub const BIDS: Map<(&str, &str), Bid> = Map::new("swap_order");
-
-// Each order bid count
-pub const ORDER_TOTAL_COUNT: Map<&str, u64> = Map::new("order_total_count");
-
-// order_id + account address -> order_count
-pub const BID_ORDER_TO_COUNT: Map<&str, u64> = Map::new("bid_order_to_count");
+pub const OBSERVATIONS: Item<Vec<Observation>> = Item::new("observations");
 
